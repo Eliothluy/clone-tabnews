66d7aca38ce1c88171474524bc9eb1a8db101be8
eliothluy <eliothluy@gmail.com>
2026-06-21 21:16:04 -0300

    Configura Next.js com dev script e página inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <h1>
+            Com muito amor e carinho coloco uma frase para você, meu amor, Jussilea.
+            Visualize e veja minha caminhada para o sucesso!
+            <br />
+            Eu amo você!
+        </h1>
+    );
+}
+
+export default Home;
