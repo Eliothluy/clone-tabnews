ca3236e6fea64445a4f036ee4e82dd88dc568f4d
eliothluy <eliothluy@gmail.com>
2026-06-23 23:38:53 -0300

    adicionado um emotion

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
             Com muito amor e carinho coloco uma frase para você, meu amor, Jussilea.
             Visualize e veja minha caminhada para o sucesso!
             <br />
-            Eu amo você!
+            Eu amo você💗!
         </h1>
     );
 }
