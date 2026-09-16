6f2863571bd2e47cd93bcd08cc92836c33c30da1
eliothluy <eliothluy@gmail.com>
2026-09-16 19:34:47 -0300

    Adiciona calculadora com testes unitarios e remove rpg-checkin

---
+++ models/calculadora.js
@@ -0,0 +1,19 @@
+function somar(numero1, numero2) {
+    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
+        return 'Erro';
+    }
+    return numero1 + numero2;
+}
+
+exports.somar = somar;
+
+
+function dividir(numero1, numero2) {
+    if (numero2 === 0) {
+        return 'Erro';
+    }
+    return numero1 / numero2;
+}
+
+
+exports.dividir = dividir;
